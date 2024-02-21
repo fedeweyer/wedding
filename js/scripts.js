da48e7cce565afedcd953e39f540d75361cd4404
Federico Weyermann <federicoaw@gmail.com>
2024-02-21 18:23:31 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbx-Ic9R6p7NZuwp2zTtTrTuNAwo3-HoCb25Oo78rKwV6gAcQH8sci2RO4epvZYWpc2i/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
